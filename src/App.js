e4b989e4341856368529eab2b06fcabdbae9b58b
Ahmad Maaz <ahmad.maaz273@gmail.com>
2023-07-13 22:59:10 +0500

    fetching dynamic data from API

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-import { Component } from 'react';
+import { Component } from "react";
 
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-class App extends Component{
-
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
 
     this.state = {
-      name: 'Ahmad Maaz',
-    }
+      monsters: [],
+    };
   }
 
-  render(){
+  //adding a life cycle method
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users#")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      });
+  }
+
+  render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name}
-          </p>
-          <button
-            onClick={()=>{
-              this.setState(()=>{
-                return {
-                  name: 'Danyaal Mahmood',
-                }
-              },()=>{
-                console.log(this.state);
-              })
-            }}
-            >
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
